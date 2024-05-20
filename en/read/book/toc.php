31773cf0a742a743736c405bc3a8db527785f9f8
Pablo Somonte Ruano <x@pablo.sx>
2024-05-20 18:20:15 +0200

    Delete en/read/book directory

--- en/read/book/toc.php
+++
@@ -1,9 +0,0 @@
-<section id="toc">
-<a href="../">🏠 Homepage</a> <br>
-<br>
-<a href="#sistas">🪐 Si(s)tas</a> <br>
-<br>
-<a href="#aural-inheritances-of-the-swahili-seas">🌊 Aural Inheritances of the Swahili Seas</a> <br>
-</section>
-<br>
-<br>
